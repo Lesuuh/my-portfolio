8b19080cea59c6a33636ef5f9d9b89e5e12bf4df
Lesuuh <lesuuh01@gmail.com>
2023-11-23 06:54:54 +0100

    added social links

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Navbar } from "./components/Navbar";
 import { Profile } from "./components/Profile";
 
+
 const App = () => {
   return (
     <div className=" py-5 px-2 w-full shadow-sm rounded bg-gray max-w-[350px] md:max-w-[1000px]  mx-auto h-auto">
