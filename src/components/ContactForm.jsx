58fdfdeb8d735a1bc6a7d5ead9303e7479bcf3a2
Lesuuh <lesuuh01@gmail.com>
2023-11-25 12:57:57 +0100

    forms

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -20,13 +20,7 @@ export const ContactForm = () => {
   };
   return (
     <div>
-      <form
-        method="post"
-        data-netlify="true"
-        name="contact"
-        onSubmit={handleSubmit}
-        className="my-5"
-      >
+      <form name="contact" onSubmit={handleSubmit} className="my-5">
         <input type="hidden" name="form-name" value="contact" />
         <div>
           <label htmlFor="name" className="font-bold poppinsfonts ">
