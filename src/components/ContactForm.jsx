90b3363407a5fa72256488f82f6e7dc74afc686c
Lesuuh <lesuuh01@gmail.com>
2023-11-25 15:12:17 +0100

    action

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -35,7 +35,7 @@ export const ContactForm = () => {
       <form
         method="POST"
         name="contact"
-        // action="/success"
+        action=""
         onSubmit={handleSubmit}
         className="my-5"
       >
